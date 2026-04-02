b168f8ce9e156bde1bc29e0667249d7e12e43c58
nhecsscehn-png <nhecsscehn@gmail.com>
2026-04-02 14:29:21 +0200

    Ajout du champs d'affichage

--- calculator/mainwindow.cpp
+++ calculator/mainwindow.cpp
@@ -12,6 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     // Rendre les boutons à nouveau formidables
 
+    QLabel *displayLbl = new QLabel();
+
     // Crée un widget central
     QWidget *centralWidget = new QWidget(this);
 
@@ -22,19 +24,26 @@ MainWindow::MainWindow(QWidget *parent)
     QVBoxLayout *vb0 = new QVBoxLayout();
     QHBoxLayout *hb0 = nullptr;
 
-    for(int m = 0; m < 9; m++){
+    for(int m = 0; m < 10; m++){
         if(m % 3 == 0){
-            hb0 = new QHBoxLayout();
-            vb0->addLayout(hb0);
+            hb0 = new QHBoxLayout(); // Pointeur nommé "hb0" qui pointe sur un nouveau conteneur horizontal
+            vb0->addLayout(hb0); // Conteneur "vb0" dans lequel l'on empile les conteneurs horizontaux
         }
-        QPushButton *numBee = new QPushButton(QString::number(m)); // Crée dynamiquement un bouton de calculatrice dont le texte affiché correspond à un chiffre. C'est typique pour éviter de créer 10 boutons manuellement (0 à 9).
+        QPushButton *numBee = new QPushButton(QString::number(m)); // Crée dynamiquement un bouton. QString::number(m) : Convertit le nombre m (0-9) en texte affichable
+        connect(numBee, &QPushButton::clicked, [numBee, displayLbl](){
+            qDebug() << numBee->text();
+            displayLbl->setText(displayLbl->text() + numBee->text());
+        });
         // numBee->setMinimumSize(80, 40);  // Taille des boutons
-        hb0->addWidget(numBee);
+        hb0->addWidget(numBee); // Placer le bouton dans le conteneur
     }
     // ui->verticalLayout->addLayout(vb0);
 
+    // Ajoute le display ou s'affiche les chiffres (ordre de ligne important pour l'endroit ou il apparaîtra)
+    mainLayout->addWidget(displayLbl);
+
     // Ajoute tous les boutons au layout principal
-    mainLayout->addLayout(vb0);
+    mainLayout->addLayout(vb0); // Parcequ'il n y a pas de verticalLayout
 
     // Définit le widget central
     setCentralWidget(centralWidget);
